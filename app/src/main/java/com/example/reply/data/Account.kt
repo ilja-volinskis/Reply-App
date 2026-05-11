d6ec8e1445c353692372a1fcc53ff2c6ff6ab8bb
DIR\ilja.volinskis <email>
2026-05-11 12:21:53 +0300

    Implement screen size responsive display navigation

--- app/src/main/java/com/example/reply/data/Account.kt
+++ app/src/main/java/com/example/reply/data/Account.kt
@@ -25,11 +25,11 @@ data class Account(
     /** Unique ID of a user **/
     val id: Long,
     /** User's first name **/
-    @StringRes val firstName: Int,
+    @param:StringRes val firstName: Int,
     /** User's last name **/
-    @StringRes val lastName: Int,
+    @param:StringRes val lastName: Int,
     /** User's email address **/
-    @StringRes val email: Int,
+    @param:StringRes val email: Int,
     /** User's avatar image resource id **/
-    @DrawableRes val avatar: Int
+    @param:DrawableRes val avatar: Int
 )
--- app/src/main/java/com/example/reply/data/Email.kt
+++ app/src/main/java/com/example/reply/data/Email.kt
@@ -28,9 +28,9 @@ data class Email(
     /** Recipient(s) of the email **/
     val recipients: List<Account> = emptyList(),
     /** Title of the email **/
-    @StringRes val subject: Int = -1,
+    @param:StringRes val subject: Int = -1,
     /** Content of the email **/
-    @StringRes val body: Int = -1,
+    @param:StringRes val body: Int = -1,
     /** Which mailbox it is in **/
     var mailbox: MailboxType = MailboxType.Inbox,
     /**
